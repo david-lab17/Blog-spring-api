92a2a1b767d6edc2368b46f7a105495de268e735
david-lab17 <davidmakuba@yahoo.com>
2022-07-29 16:24:50 +0300

    Adding Swagger Documentation

--- src/main/java/com/deltacode/springbootblog/config/SpringFoxConfig.java
+++ src/main/java/com/deltacode/springbootblog/config/SpringFoxConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -12,12 +14,25 @@ public class SpringFoxConfig {
     @Bean
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+                   .select()
+                .paths(PathSelectors.ant("/api/**"))
+                .apis(RequestHandlerSelectors.basePackage("com.deltacode.springbootblog"))
+                .build()
+                .apiInfo(apiInfo());
 
-                .build();
-
+    }
+    private ApiInfo apiInfo(){
+        ApiInfo apiInfo = new ApiInfo(
+                "Blog API",
+                "Spring Boot REST API for Blog Application",
+                "1.0",
+                "Free to use",
+                new Contact("David Charo", "https://agitated-tesla-ccf7b9.netlify.app/", "davidcharomakuba@fmail.com"),
+                "Api License",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                new java.util.ArrayList<>()
+        );
+        return apiInfo;
     }
 
 
